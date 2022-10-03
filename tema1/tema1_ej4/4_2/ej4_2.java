79d33cfae3765fede8dc02d7a757004980ab0603
laura <laurajorques2003@gmail.com>
2022-10-03 18:36:17 +0200

    Ejercicio 4_2 hecho

--- tema1/tema1_ej4/4_2/ej4_2.java
+++ tema1/tema1_ej4/4_2/ej4_2.java
@@ -1,5 +1,21 @@
+import java.io.FileWriter;
+import java.util.Scanner;
+
 public class ej4_2 {
     public static void main(String[] args) {
-
+        FileWriter fichero;
+        Scanner read = new Scanner(System.in);
+        String cadena;
+        try {
+            fichero = new FileWriter("tema1/tema1_ej4/4_2/anotaciones.txt", true);
+            do {
+                System.out.println("Introduce una cadena, cuando quieras finalizar no escribas nada");
+                cadena = read.nextLine();
+                fichero.write(cadena+ "\n");
+            } while (!cadena.isEmpty());
+            fichero.close();
+        } catch (Exception e) {
+            System.out.println("Ha habido un error");
+        }
     }
 }
