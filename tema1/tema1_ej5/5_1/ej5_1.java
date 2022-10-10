151bf6c089bd9a06532e308addfd55b06c7d31d9
laura <laurajorques2003@gmail.com>
2022-10-10 17:03:47 +0200

    Ejercicio 5_2 acabado y 5_1 mejorado

--- tema1/tema1_ej5/5_1/ej5_1.java
+++ tema1/tema1_ej5/5_1/ej5_1.java
@@ -1,6 +1,8 @@
 import java.io.*;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class ej5_1 {
     public static void main(String[] args) {
@@ -8,15 +10,13 @@ public class ej5_1 {
         try (FileOutputStream fos = new FileOutputStream(fichero.toFile());
             ObjectOutputStream oos = new ObjectOutputStream(fos)) {
 
-            Date fecha1 = new Date(1980-9-25);
-            Date fecha2 = new Date(2003-1-15);
-            Date fecha3 = new Date(2000-11-3);
-
-            Persona pers = new Persona("Roberto", "roberto1980@gmail.com", fecha1);
-            oos.writeObject(pers);
-            pers = new Persona("Juliana", "juls_lopez@gmail.com", fecha2);
-            oos.writeObject(pers);
-            pers = new Persona("Toni", "garciatoni@gmail.com", fecha3);
+            Date fecha1 = new Date(80,9,20);
+            Date fecha2 = new Date(103,1,15);
+            Date fecha3 = new Date(100,11,3);
+            List<Persona> pers = new ArrayList<>();
+            pers.add(new Persona("Roberto", "roberto1980@gmail.com", fecha1));
+            pers.add(new Persona("Juliana", "juls_lopez@gmail.com", fecha2));
+            pers.add(new Persona("Toni", "garciatoni@gmail.com", fecha3));
             oos.writeObject(pers);
 
         } catch (Exception ex) {
--- tema1/tema1_ej5/5_2/ej5_2.java
+++ tema1/tema1_ej5/5_2/ej5_2.java
@@ -1,13 +1,16 @@
 import java.io.FileInputStream;
 import java.io.ObjectInputStream;
 import java.nio.file.Path;
+import java.util.List;
 
 public class ej5_2 {
     public static void main(String[] args) {
         Path fichero = Path.of("tema1/tema1_ej5/5_1", "personas.dat");
         try (FileInputStream fis = new FileInputStream(fichero.toFile()); ObjectInputStream ois = new ObjectInputStream(fis)) {
-            Persona p = (Persona) ois.readObject();
-            p.escribir();
+            List<Persona> pers = (List<Persona>) (ois.readObject());
+            for (Persona p: pers) {
+                p.escribir();
+            }
         } catch (Exception ex) {
             System.out.println("Se ha encontrado un error");
         }
