7e7e04ed47b73b8fdaeb6ff2f58f49c4be0e3086
laura <laurajorques2003@gmail.com>
2022-10-10 18:56:42 +0200

    5_3 finalizado

--- tema1/tema1_ej5/5_3/ej5_3.java
+++ tema1/tema1_ej5/5_3/ej5_3.java
@@ -1,5 +1,8 @@
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -10,45 +13,82 @@ import java.util.Scanner;
 public class ej5_3 {
     public static void main(String[] args) {
         Scanner read = new Scanner(System.in);
+        int opcion;
+
+        do {
+            System.out.println("""
+                    -----------MENU-----------
+                    1. Introducir datos de personas
+                    2. Buscar entre los datos existentes
+                    3. Cargar todos
+                    4. Salir""");
+            opcion =  read.nextInt();
+            read.nextLine();
+            if (opcion == 1) {
+                guardarPersona();
+            } else if (opcion == 2) {
+                buscarPersona();
+            } else if (opcion == 3) {
+                cargarPersonas();
+            } else if (opcion > 4) {
+                System.out.println("Opción no disponible");
+            }
+        } while (opcion != 4);
+    }
+    public static void guardarPersona() {
+        Scanner read = new Scanner(System.in);
         Path fichero = Path.of("tema1/tema1_ej5/5_3", "personas.dat");
         String name;
         String email;
         String fecha;
         Date date;
-        int opcion;
         SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
         List<Persona> pers = new ArrayList<>();
-        do {
-            System.out.println("-----------MENU-----------\n" +
-                    "1. Introducir datos de personas\n" +
-                    "2. Buscar entre los datos existentes\n" +
-                    "3. Salir");
-            opcion =  read.nextInt();
-            try (FileOutputStream fos = new FileOutputStream(fichero.toFile());
-                 ObjectOutputStream oos = new ObjectOutputStream(fos)) {
-                if (opcion >=1 && opcion <=3) {
-                    if (opcion == 1) {
-                        System.out.println("Has seleccionado añadir persona");
-                        System.out.print("Nombre: ");
-                        name = read.nextLine();
-                        System.out.print("Email: ");
-                        email = read.nextLine();
-                        System.out.print("Fecha de nacimiento (dd/MM/yyyy): ");
-                        fecha = read.nextLine();
-                        date = formato.parse(fecha);
-                        pers.add(new Persona(name, email, date));
-                        oos.writeObject(pers);
-                    } else if (opcion == 2) {
+        try (FileOutputStream fos = new FileOutputStream(fichero.toFile());
+             ObjectOutputStream oos = new ObjectOutputStream(fos)) {
+            System.out.println("Has seleccionado añadir persona");
+            System.out.print("Nombre: ");
+            name = read.nextLine();
+            System.out.print("Email: ");
+            email = read.nextLine();
+            System.out.print("Fecha de nacimiento (dd/MM/yyyy): ");
+            fecha = read.nextLine();
+            date = formato.parse(fecha);
+            pers.add(new Persona(name, email, date));
+            oos.writeObject(pers);
+        } catch (Exception ex) {
+            System.out.println("Se ha encontrado un error");
+        }
+    }
 
-                    }
+    public static void buscarPersona() {
+        Scanner read = new Scanner(System.in);
+        String name2;
+        Path fichero = Path.of("tema1/tema1_ej5/5_3", "personas.dat");
+        try (FileInputStream fis = new FileInputStream(fichero.toFile()); ObjectInputStream ois = new ObjectInputStream(fis)) {
+            List<Persona> pers = (List<Persona>) (ois.readObject());
+            System.out.println("Escribe el nombre que quieres buscar");
+            name2= read.nextLine();
+            for (Persona p: pers) {
+                if (pers.contains(name2)) {
+                    p.escribir();
                 } else {
-                    System.out.println("Opción no disponible");
+                    System.out.println("probando");
                 }
-
-            } catch (Exception ex) {
-                System.out.println("Se ha encontrado un error");
             }
-        } while(opcion != 3);
-        System.out.println("Has salido.");
+        } catch (Exception ex) {
+            System.out.println("Se ha encontrado un error");
+        }
+    }
+    public static void cargarPersonas() {
+        Path fichero = Path.of("tema1/tema1_ej5/5_3", "personas.dat");
+        try (FileInputStream fis = new FileInputStream(fichero.toFile()); ObjectInputStream ois = new ObjectInputStream(fis)) {
+            List<Persona> pers = (List<Persona>) (ois.readObject());
+            for (Persona p: pers) {
+                p.escribir();
+            }
+        } catch (Exception ex) {
+            System.out.println("Se ha encontrado un error");
+        }
     }
 }
