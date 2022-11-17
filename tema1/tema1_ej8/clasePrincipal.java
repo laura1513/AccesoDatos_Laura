91b7668488a4b6aac2ce5e6e097bd16714562d4b
laura1513 <laurajorques2003@gmail.com>
2022-11-17 16:43:16 +0100

    acabado tema01/tema1_ej8

--- tema1/tema1_ej8/clasePrincipal.java
+++ tema1/tema1_ej8/clasePrincipal.java
@@ -15,8 +15,8 @@ import java.util.Objects;
 public class clasePrincipal {
     private static final String COMMA_DELIMITER=",";
     public static void main(String[] args) {
-        /*List<race> races = new ArrayList<>();
-        List<sprint> sprint = new ArrayList<>();*/
+        List<race> races = new ArrayList<>();
+        List<sprint> sprint = new ArrayList<>();
         Path fichero = Path.of("tema1/tema1_ej8/formula1_2021season_calendar.xml");
         JAXBContext context = null;
         try (FileReader fr = new FileReader("tema1/tema1_ej8/formula1_2021season_raceResults.csv");
@@ -29,17 +29,60 @@ public class clasePrincipal {
 
             arrayListCalendario cl = (arrayListCalendario) jaxbUnmarshaller.unmarshal(fichero.toFile());
 
-            ArrayList<circuito> c = cl.getCls();
-            for (circuito ci : cl.getCls()) {
+            String linea;
+            br.readLine();
+            /*for (circuito ci : cl.getCls()) {
                 System.out.println(ci.toString());
+            }*/
+            while ((linea = br.readLine()) != null) {
+                String[] piloto = linea.split(COMMA_DELIMITER);
+                race ra = new race();
+                for (circuito ci : cl.getCls()) {
+                    if (piloto[0].equalsIgnoreCase(ci.getGpname())) {
+                        ra.setTrack(ci);
+                    }
+                }
+                if (Objects.equals(piloto[1], "NC")) {
+                    ra.setPosition("0");
+                } else if (Objects.equals(piloto[1], "DQ")){
+                    ra.setPosition("0");
+                }else {
+                    ra.setPosition(piloto[1]);
+                }
+                ra.setNo(Integer.parseInt(piloto[2]));
+                ra.setDriver(piloto[3]);
+                ra.setTeam(piloto[4]);
+                ra.setStartingGrid(Integer.parseInt(piloto[5]));
+                ra.setLaps(Integer.parseInt(piloto[6]));
+                ra.setTime(piloto[7]);
+                ra.setPoints(Double.parseDouble(piloto[8]));
+                ra.setExtraPoints(piloto[9]);
+                ra.setFastestLap(piloto[10]);
+                races.add(ra);
             }
-            /*HashMap<String, String> ganadorPremio = new HashMap<>();
+            System.out.println("Un listado de cada gran premio y su ganador.");
+            System.out.println();
+            HashMap<String, String> ganadorPremio = new HashMap<>();
             for (race ci : races) {
                 if(!ganadorPremio.containsKey(ci.getTrack().getGpname())) {
                     ganadorPremio.put(ci.getTrack().getGpname(), ci.getDriver());
                 }
             }
-            ganadorPremio.entrySet().stream().forEach(System.out::println);*/
+            ganadorPremio.entrySet().stream().forEach(System.out::println);
+
+            System.out.println();
+
+            System.out.println("Un listado del número de grandes premios celebrados por país.");
+            System.out.println();
+            HashMap<String,Integer> premiosPorPais = new HashMap<>();
+            for (circuito circ : cl.getCls()) {
+                if (!premiosPorPais.containsKey(circ.getCountry())) {
+                    premiosPorPais.put(circ.getCountry(), 1);
+                } else {
+                    premiosPorPais.put(circ.getCountry(), premiosPorPais.get(circ.getCountry())+1);
+                }
+            }
+            premiosPorPais.entrySet().stream().filter(p->p.getValue()>=0).forEach(System.out::println);
         }catch(JAXBException | IOException e) {
             e.printStackTrace();
         }
